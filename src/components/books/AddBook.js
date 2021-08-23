30b87ebe05262641f629b317edb9296a3bcea0ce
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-23 10:18:26 -0500

    Add options to select by map

--- src/components/books/AddBook.js
+++ src/components/books/AddBook.js
@@ -1,27 +1,13 @@
 import React from 'react';
+import options from './options';
 
 const AddBook = () => (
   <form>
     <input name="title" placeholder="Add a title book" />
     <select defaultValue="category">
       <option value="category" disabled>Category</option>
-      <option value="technology">Technology</option>
-      <option value="fiction">Fiction</option>
-      <option value="fantasy">Fantasy</option>
-      <option value="romance">Romance</option>
-      <option value="contemporary">Contemporary</option>
-      <option value="mystery">Mystery</option>
-      <option value="horror">Horror</option>
-      <option value="paranormal">Paranormal</option>
-      <option value="science">Science</option>
-      <option value="art">Art</option>
-      <option value="cooking">Cooking</option>
-      <option value="motivational">Motivational</option>
-      <option value="history">History</option>
-      <option value="health">Health</option>
-      <option value="humor">Humor</option>
-      <option value="travel">Travel</option>
-      <option value="guide">Guide/How-to</option>
+      {options.map((option) => <option key={option.id} value={option.value}>{option.text}</option>)}
+
     </select>
     <button type="submit">ADD BOOK</button>
   </form>
---
+++ src/components/books/options.js
@@ -0,0 +1,89 @@
+const options = [
+  {
+    text: 'Technology',
+    value: 'technology',
+    id: '1',
+  },
+  {
+    text: 'Fiction',
+    value: 'fiction',
+    id: '2',
+  },
+  {
+    text: 'Fantasy',
+    value: 'fantasy',
+    id: '3',
+  },
+  {
+    text: 'Romance',
+    value: 'romance',
+    id: '4',
+  },
+  {
+    text: 'Contemporary',
+    value: 'contemporary',
+    id: '5',
+  },
+  {
+    text: 'Mystery',
+    value: 'mystery',
+    id: '6',
+  },
+  {
+    text: 'Horror',
+    value: 'horror',
+    id: '7',
+  },
+  {
+    text: 'Paranormal',
+    value: 'paranormal',
+    id: '8',
+  },
+  {
+    text: 'Science',
+    value: 'science',
+    id: '9',
+  },
+  {
+    text: 'Art',
+    value: 'art',
+    id: '10',
+  },
+  {
+    text: 'Cooking',
+    value: 'cooking',
+    id: '11',
+  },
+  {
+    text: 'Motivational',
+    value: 'motivational',
+    id: '12',
+  },
+  {
+    text: 'History',
+    value: 'history',
+    id: '13',
+  },
+  {
+    text: 'Health',
+    value: 'health',
+    id: '14',
+  },
+  {
+    text: 'Humor',
+    value: 'humor',
+    id: '15',
+  },
+  {
+    text: 'Travel',
+    value: 'travel',
+    id: '16',
+  },
+  {
+    text: 'Guide/How-to',
+    value: 'guide',
+    id: '17',
+  },
+];
+
+export default options;
