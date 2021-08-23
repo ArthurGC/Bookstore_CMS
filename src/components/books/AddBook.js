3aaf123716fb863ae7586c7f1cd1c602a8454670
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-23 00:09:50 -0500

    Add addBook form to submit a new book

---
+++ src/components/books/AddBook.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const AddBook = () => (
+  <form>
+    <input name="title" placeholder="Add a title book" />
+    <select>
+      <option value="category" disabled>Category</option>
+      <option value="technology">Technology</option>
+      <option value="fiction">Fiction</option>
+      <option value="fantasy">Fantasy</option>
+      <option value="romance">Romance</option>
+      <option value="contemporary">Contemporary</option>
+      <option value="mystery">Mystery</option>
+      <option value="horror">Horror</option>
+      <option value="paranormal">Paranormal</option>
+      <option value="science">Science</option>
+      <option value="art">Art</option>
+      <option value="cooking">Cooking</option>
+      <option value="motivational">Motivational</option>
+      <option value="history">History</option>
+      <option value="health">Health</option>
+      <option value="humor">Humor</option>
+      <option value="travel">Travel</option>
+      <option value="guide">Guide/How-to</option>
+    </select>
+    <button type="submit">ADD BOOK</button>
+  </form>
+);
+
+export default AddBook;
