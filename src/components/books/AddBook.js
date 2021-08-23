24b5658e2993cdcc8d515b1aa2df643771ccac70
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-23 11:41:12 -0500

    Add dispatch to addBookAction

--- src/components/books/AddBook.js
+++ src/components/books/AddBook.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBookAction } from '../../redux/books/books';
 import options from './options';
 
-const AddBook = () => (
-  <form>
-    <input name="title" placeholder="Add a title book" />
-    <select defaultValue="category">
-      <option value="category" disabled>Category</option>
-      {options.map((option) => <option key={option.id} value={option.value}>{option.text}</option>)}
+const AddBook = () => {
+  const dispatch = useDispatch();
 
-    </select>
-    <button type="submit">ADD BOOK</button>
-  </form>
-);
+  const submitBooktoStore = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const author = e.target.author.value;
+    e.target.title.value = '';
+    e.target.author.value = '';
+    dispatch(addBookAction(title, author));
+  };
+
+  return (
+    <form onSubmit={submitBooktoStore}>
+      <input name="title" placeholder="Add a book" />
+      <input name="author" placeholder="Add a author" />
+      <select defaultValue="category">
+        <option value="category" disabled>Category</option>
+        {options.map((option) => (
+          <option
+            key={option.id}
+            value={option.value}
+          >
+            {option.text}
+          </option>
+        ))}
+
+      </select>
+      <button type="submit">ADD BOOK</button>
+    </form>
+  );
+};
 
 export default AddBook;
