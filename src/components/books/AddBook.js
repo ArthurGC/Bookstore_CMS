d10d94af81e2aef10a6d70344b1c8cc7b9852645
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-24 13:14:47 -0500

    Add styles to addNewBook form

--- src/components/books/AddBook.js
+++ src/components/books/AddBook.js
@@ -16,22 +16,27 @@ const AddBook = () => {
   };
 
   return (
-    <form onSubmit={submitBooktoStore}>
-      <input name="title" placeholder="Add a book" />
-      <select name="category" defaultValue="technology">
-        <option value="category:" disabled>Category</option>
-        {options.map((option) => (
-          <option
-            key={option.id}
-            value={option.value}
-          >
-            {option.text}
-          </option>
-        ))}
+    <div className="px-8 sm:px-16 lg:px-24 xl:px-28">
+      <div className="container mx-auto">
+        <h2 className="text-add font-mont font-bold text-xl mb-4">ADD NEW BOOK</h2>
+        <form onSubmit={submitBooktoStore} className="grid lg:grid-cols-5 lg:gap-8">
+          <input name="title" placeholder="Book title" className="px-4 py-3 rounded lg:col-span-3 border border-gray-200 font-mont font-normal text-base" />
+          <select name="category" defaultValue="technology" className="bg-white px-4 py-3 rounded border border-gray-200 font-mont font-normal text-base">
+            <option value="category:" disabled>Category</option>
+            {options.map((option) => (
+              <option
+                key={option.id}
+                value={option.value}
+              >
+                {option.text}
+              </option>
+            ))}
 
-      </select>
-      <button type="submit">ADD BOOK</button>
-    </form>
+          </select>
+          <button type="submit" className="bg-azure text-white py-3 px-11 rounded font-robo font-bold text-sm hover:bg-blue-600">ADD BOOK</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
