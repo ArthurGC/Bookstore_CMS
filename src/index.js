140aedc5f592f205f3c959c4955a64efec2e38a9
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-24 10:08:22 -0500

    Add tailwind

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './redux/configureStore';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
