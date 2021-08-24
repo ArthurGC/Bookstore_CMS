b327a94247610c15d3b3fbb5eb5b8f45c6d64be9
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-24 13:15:32 -0500

    Add styles to BookList component

--- src/components/books/BookList.js
+++ src/components/books/BookList.js
@@ -9,13 +9,18 @@ const BookList = () => {
   useBooks();
 
   return (
-    <ul className="booklist__container">
-      {books.map((book) => (
-        <li key={book.item_id}>
-          <Book title={book.title} category={book.category} id={book.item_id} />
-        </li>
-      ))}
-    </ul>
+    <div className="px-8 sm:px-16 lg:px-24 xl:px-28 bg-backg py-9">
+      <ul className="container mx-auto">
+        {books.map((book) => (
+          <li
+            key={book.item_id}
+            className="bg-white px-7 py-8 rounded mb-4"
+          >
+            <Book title={book.title} category={book.category} id={book.item_id} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
